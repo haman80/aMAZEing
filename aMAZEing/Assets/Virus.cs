1ae29f843f81d96e94d17f33e4eb78db34602f2a
ivliu816 <ivliu816@gmail.com>
2022-12-18 00:51:02 -0500

    revert back to previous changes

--- aMAZEing/Assets/Virus.cs
+++ aMAZEing/Assets/Virus.cs
@@ -1,72 +1,72 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// // functionality of virus
-// // animates color and size of the virus
-// // and attacks the player if the player is near the virus (check the code)
-// public class Virus : MonoBehaviour
-// {
-//     private GameObject fps_player_obj;
-//     private Level level;
-//     private AudioSource src;
-//     private float radius_of_search_for_player;
-//     private float virus_speed;
+// functionality of virus
+// animates color and size of the virus
+// and attacks the player if the player is near the virus (check the code)
+public class Virus : MonoBehaviour
+{
+    private GameObject fps_player_obj;
+    private Level level;
+    private AudioSource src;
+    private float radius_of_search_for_player;
+    private float virus_speed;
 
-// 	void Start ()
-//     {
-//         GameObject level_obj = GameObject.FindGameObjectWithTag("Level");
-//         level = level_obj.GetComponent<Level>();
-//         if (level == null)
-//         {
-//             Debug.LogError("Internal error: could not find the Level object - did you remove its 'Level' tag?");
-//             return;
-//         }
-//         fps_player_obj = level.fps_player_obj;
-//         Bounds bounds = level.GetComponent<Collider>().bounds;
-//         radius_of_search_for_player = (bounds.size.x + bounds.size.z) / 15.0f;
-//         virus_speed = level.virus_speed;
-//         src = level.source;
-//     }
+	void Start ()
+    {
+        GameObject level_obj = GameObject.FindGameObjectWithTag("Level");
+        level = level_obj.GetComponent<Level>();
+        if (level == null)
+        {
+            Debug.LogError("Internal error: could not find the Level object - did you remove its 'Level' tag?");
+            return;
+        }
+        fps_player_obj = level.fps_player_obj;
+        Bounds bounds = level.GetComponent<Collider>().bounds;
+        radius_of_search_for_player = (bounds.size.x + bounds.size.z) / 15.0f;
+        virus_speed = level.virus_speed;
+        src = level.source;
+    }
 
-//     // *** YOU NEED TO COMPLETE THIS PART OF THE FUNCTION TO ANIMATE THE VIRUS ***
-//     // so that it moves towards the player when the player is within radius_of_search_for_player
-//     // a simple strategy is to update the position of the virus
-//     // so that it moves towards the direction d=v/||v||, where v=(fps_player_obj.transform.position - transform.position)
-//     // with rate of change (virus_speed * Time.deltaTime)
-//     // make also sure that the virus y-coordinate position does not go above the wall height
-//     void Update()
-//     {
-//         if (level.player_health < 0.001f || level.player_entered_house)
-//             return;
-//         Color redness = new Color
-//         {
-//             r = Mathf.Max(1.0f, 0.25f + Mathf.Abs(Mathf.Sin(2.0f * Time.time)))
-//         };
-//         // if ( transform.childCount > 0)
-//         //     transform.GetChild(0).GetComponent<MeshRenderer>().material.color = redness;
-//         // else
-//         //     transform.GetComponent<MeshRenderer>().material.color = redness;
-//         transform.localScale = new Vector3(
-//                                0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time)), 
-//                                0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time)), 
-//                                0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time))
-//                                );
-//         Vector3 v = (fps_player_obj.transform.position - transform.position);
-//         Vector3 d = v/v.magnitude;
-//         if(v.magnitude < radius_of_search_for_player)
-//             transform.position += d * (virus_speed * Time.deltaTime);
-//         transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
-//     }
+    // *** YOU NEED TO COMPLETE THIS PART OF THE FUNCTION TO ANIMATE THE VIRUS ***
+    // so that it moves towards the player when the player is within radius_of_search_for_player
+    // a simple strategy is to update the position of the virus
+    // so that it moves towards the direction d=v/||v||, where v=(fps_player_obj.transform.position - transform.position)
+    // with rate of change (virus_speed * Time.deltaTime)
+    // make also sure that the virus y-coordinate position does not go above the wall height
+    void Update()
+    {
+        if (level.player_health < 0.001f || level.player_entered_house)
+            return;
+        Color redness = new Color
+        {
+            r = Mathf.Max(1.0f, 0.25f + Mathf.Abs(Mathf.Sin(2.0f * Time.time)))
+        };
+        if ( transform.childCount > 0)
+            transform.GetChild(0).GetComponent<MeshRenderer>().material.color = redness;
+        else
+            transform.GetComponent<MeshRenderer>().material.color = redness;
+        transform.localScale = new Vector3(
+                               0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time)), 
+                               0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time)), 
+                               0.9f + 0.2f * Mathf.Abs(Mathf.Sin(4.0f * Time.time))
+                               );
+        Vector3 v = (fps_player_obj.transform.position - transform.position);
+        Vector3 d = v/v.magnitude;
+        if(v.magnitude < radius_of_search_for_player)
+            transform.position += d * (virus_speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
+    }
 
-//     private void OnCollisionEnter(Collision collision)
-//     {
-//         if (collision.gameObject.name == "PLAYER")
-//         {
-//             level.virus_landed_on_player_recently = true;
-//             src.PlayOneShot(level.infected);
-//             Destroy(gameObject);
-//         }
-//     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PLAYER")
+        {
+            level.virus_landed_on_player_recently = true;
+            src.PlayOneShot(level.infected);
+            Destroy(gameObject);
+        }
+    }
     
-// }
+}
